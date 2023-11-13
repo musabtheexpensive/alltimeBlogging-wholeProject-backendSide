67fb6ee06f309911e49480d568d6fc3cc041d583
Musab Al Mahi <musabalmahi53@gmail.com>
2023-11-13 17:15:01 +0600

    added the JWT token but removed it again due to unavoidable reasons

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
+// const jwt = require("jsonwebtoken");
+// const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,11 +16,11 @@ app.use(
       'https://assignment11-blog-website.web.app',
       'https://assignment11-blog-website.firebaseapp.com'
     ],
-    credentials: true,
+  
   })
 );
 app.use(express.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uak4fm8.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,65 +34,66 @@ const client = new MongoClient(uri, {
 });
 
 // my middlewares
-const logger = (req, res, next) => {
-  console.log("log: info", req.host, req.originalUrl);
-  next();
-};
+// const logger = (req, res, next) => {
+//   console.log("log: info", req.host, req.originalUrl);
+//   next();
+// };
 
-const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log("value of token in middleware", token);
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    // error
-    if (err) {
-      console.log(err);
-      return res.status(401).send({ message: "unauthorized access" });
-    }
-    // if token is valid then it would be decoded
-    console.log("value in the token", decoded);
-    req.user = decoded;
-    next();
-  });
-};
+// const verifyToken = (req, res, next) => {
+//   const token = req.cookies?.token;
+//   console.log("value of token in middleware", token);
+//   if (!token) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     // error
+//     if (err) {
+//       console.log(err);
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
+//     // if token is valid then it would be decoded
+//     console.log("value in the token", decoded);
+//     req.user = decoded;
+//     next();
+//   });
+// };
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const blogCollection = client.db("allBlogs").collection("blogByUser");
     const wishlistCollection = client.db("allBlogs").collection("wishlist");
 
-    // Auth Related Api Start Here
-    app.post("/jwt", logger, async (req, res) => {
-      const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-      res
-        // // token set code here
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: false, // http://localhost:5173/login
-          // sameSite: "none",
-        })
-        .send({ success: true });
-    });
+    // // Auth Related Api Start Here
+    // app.post("/jwt", logger, async (req, res) => {
+    //   const user = req.body;
+    //   console.log(user);
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+    //   res
+    //     // // token set code here
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: false, // http://localhost:5173/login
+    //       // sameSite: "none",
+    //     })
+    //     .send({ success: true });
+    // });
 
-     // when user logout then remove token. this code start here
-     app.post("/logout", async (req, res) => {
-      const user = req.body;
-      console.log("logging out", user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
-    });
+    //  // when user logout then remove token. this code start here
+    //  app.post("/logout", async (req, res) => {
+    //   const user = req.body;
+    //   console.log("logging out", user);
+    //   res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    // });
 
 
-    //backend create operation
-    app.post("/blogByUser", logger, async (req, res) => {
+    //backend create operation start here
+    // ekhane logger use hobe async er aagge
+    app.post("/blogByUser",  async (req, res) => {
       const newBlog = req.body;
       console.log(newBlog);
       const result = await blogCollection.insertOne(newBlog);
@@ -136,12 +137,13 @@ async function run() {
     });
 
     // wishlist all by specific user read and
-    app.get("/wishlist", logger, verifyToken, async (req, res) => {
+    // ekhane async er aagge logger o verify use hobe
+    app.get("/wishlist",  async (req, res) => {
       // console.log("The Token IS=", req.cookies.token);
-      console.log("user in the valid token",req.user);
-      if (req.user.email !== req.query.email) {
-        return res.status(403).send({ message: "forbidden access" });
-      }
+      // console.log("user in the valid token",req.user);
+      // if (req.user.email !== req.query.email) {
+      //   return res.status(403).send({ message: "forbidden access" });
+      // }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -159,7 +161,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
